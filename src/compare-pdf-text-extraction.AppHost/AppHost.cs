43be55013e521f42a4009184830bf9ed81fd622b
Kijana Woodard <kijana.woodard@kaizen.io>
2025-12-16 17:16:53 -0600

    aspirify

--- src/compare-pdf-text-extraction.AppHost/AppHost.cs
+++ src/compare-pdf-text-extraction.AppHost/AppHost.cs
@@ -1,3 +1,25 @@
 var builder = DistributedApplication.CreateBuilder(args);
+const string appName = "compare-pdf-text-extraction";
+
+var pythonApi = builder
+    .AddUvicornApp("python-api", "../python_api", "python_api:app")
+    .WithUv()
+    .WithEndpoint("http", e => e.UriScheme  = builder.ExecutionContext.IsPublishMode ? "https" : "http")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health");
+
+if (!builder.ExecutionContext.IsPublishMode)
+    builder.CreateResourceBuilder((ExecutableResource)builder.Resources.First(x => x.Name == $"{pythonApi.Resource.Name}-installer"))
+        .WithCertificateTrustScope(CertificateTrustScope.None);
+
+var csharpApi = builder.AddProject<Projects.CsharpApi>("csharp-api");
+
+builder.AddProject<Projects.Web>("web")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(pythonApi)
+    .WithReference(csharpApi)
+    .WaitFor(pythonApi)
+    .WaitFor(csharpApi);
 
 builder.Build().Run();
