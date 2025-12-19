8d566c14c0c418ead83107fe7433df37122a0e13
Kijana Woodard <kijana.woodard@kaizen.io>
2025-12-18 18:48:28 -0600

    target LinuxArm64

--- src/compare-pdf-text-extraction.AppHost/AppHost.cs
+++ src/compare-pdf-text-extraction.AppHost/AppHost.cs
@@ -1,6 +1,8 @@
+using Aspire.Hosting.Publishing;
+
 var builder = DistributedApplication.CreateBuilder(args);
 const string appName = "compare-pdf-text-extraction";
-
+#pragma warning disable ASPIREPIPELINES003
 builder.AddDockerComposeEnvironment("env")
     .WithDashboard(db => db.WithHostPort(8085))
     .WithSshDeploySupport();
@@ -8,14 +10,17 @@ builder.AddDockerComposeEnvironment("env")
 var pythonApi = builder
     .AddUvicornApp("python-api", "../python_api", "python_api:app")
     .WithUv()
+    .WithContainerBuildOptions(context => context.TargetPlatform = ContainerTargetPlatform.LinuxArm64)
     //.WithEndpoint("http", e => e.UriScheme  = builder.ExecutionContext.IsPublishMode ? "https" : "http")
     //.WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health");
 
 var csharpApi = builder.AddProject<Projects.CsharpApi>("csharp-api")
+    .WithContainerBuildOptions(context => context.TargetPlatform = ContainerTargetPlatform.LinuxArm64)
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.Web>("web")
+    .WithContainerBuildOptions(context => context.TargetPlatform = ContainerTargetPlatform.LinuxArm64)
     .WithEndpoint("http", e => e.Port = 59436)
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
